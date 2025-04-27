499a76ff4082fa911af4938d06fd6cee495831b3
somea82 <joemoon0526@gmail.com>
2025-04-27 16:30:36 +0900

    fix: get course  수정 schoolId 필요없음

--- src/main/java/com/example/kummiRoom_backend/api/controller/CourseController.java
+++ src/main/java/com/example/kummiRoom_backend/api/controller/CourseController.java
@@ -1,13 +1,20 @@
 package com.example.kummiRoom_backend.api.controller;
 
 import com.example.kummiRoom_backend.api.dto.responseDto.CourseResponseDto;
+import com.example.kummiRoom_backend.api.entity.User;
+import com.example.kummiRoom_backend.api.repository.UserRepository;
 import com.example.kummiRoom_backend.api.service.CourseService;
 import com.example.kummiRoom_backend.global.apiResult.ApiResult;
+import com.example.kummiRoom_backend.global.auth.AuthService;
+import com.example.kummiRoom_backend.global.auth.JwtService;
+import com.example.kummiRoom_backend.global.exception.UnauthorizedException;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,16 +22,27 @@ import java.util.List;
 public class CourseController {
 
     private final CourseService courseService;
+    private final AuthService authService;
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
 
     @GetMapping
-    public ResponseEntity<?> getCoursesBySchool(@RequestParam("schoolId") Long schoolId) {
-        List<CourseResponseDto> response = courseService.getCoursesBySchoolId(schoolId);
+    public ResponseEntity<?> getCoursesBySchool(HttpServletRequest request) {
+        String accessToken = authService.getCookieValue(request, "accessToken");
+        if(accessToken == null){
+            throw new UnauthorizedException("액세스 토큰이 없습니다");
+        }
+        Long userId = jwtService.extractUserId(accessToken);
+
+        Optional<User> user = userRepository.findById(userId);
+
+        List<CourseResponseDto> response = courseService.getCoursesBySchoolId(user.get().getSchool().getSchoolId());
         return ResponseEntity.ok(new ApiResult(200,"OK","학교 정보 조회에 성공하였습니다.",response));
     }
 
-    @GetMapping("/{course_id}")
-    public ResponseEntity<?> getCourseDetailById(@RequestParam("school_id") Long schoolId,
-                                                 @PathVariable("course_id") Long courseId) {
+    @GetMapping("/{courseId}")
+    public ResponseEntity<?> getCourseDetailById(@RequestParam("schoolId") Long schoolId,
+                                                 @PathVariable("courseId") Long courseId) {
         CourseResponseDto response = courseService.getCourseBySchoolIdAndCourseId(schoolId, courseId);
         return ResponseEntity.ok(new ApiResult(200, "OK", "과목 상세 조회에 성공하였습니다.", response));
     }
